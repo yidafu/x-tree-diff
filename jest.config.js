629826daa5f16c915499598b0327412b3b2ff765
yidafu <tiaozuwushi@qq.com>
2019-12-26 20:06:48 +0800

    chore: add jest, .editorconfig

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
+const { defaults } = require('jest-config');
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'mjs', 'ts'],
 };
