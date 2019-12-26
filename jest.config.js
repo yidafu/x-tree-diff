c25c766ec69922bb7dd154758460e1927ec0dc78
yidafu <tiaozuwushi@qq.com>
2019-12-26 22:51:31 +0800

    chore: update eslint rules/tsconfig.json

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,4 @@
-const { defaults } = require('jest-config');
-
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  moduleFileExtensions: [...defaults.moduleFileExtensions, 'mjs', 'ts'],
 };
