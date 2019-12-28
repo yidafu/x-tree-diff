33235f84b1106bebdac38f850d4c38b73725cd6f
yidafu <tiauzuwushi@qq.com>
2019-12-28 14:50:23 +0800

    feat: index.ts to export classes and enum

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as XTree, NodeType } from './XTree';
+export { default as XTreeDiff } from './XTreeDiff';
+export { default as XMLXTreeDiff } from './XMLXTreeDiff';
+export { default as HTMLXTreeDiff } from './HTMLXTreeDiff';
+export { default as EditOption } from './EditOption';
