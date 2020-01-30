62545a18d5cc2ee33f08623e23e5dde654606e4d
yidafu <tiauzuwushi@qq.com>
2020-01-30 13:13:58 +0800

    chore: release cli

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,6 +5,7 @@ module.exports = {
       2, 'always', [
         'build', 'chore', 'ci', 'docs', 'feat', 'fix', 'merge', 'perf',
         'refactor', 'revert', 'style', 'test', 'func', 'design', 'init',
+        'release',
       ],
     ],
   },
